7d3873dcd01a0310339487ff6eb682f85ed21de7
YuxinWang2020 <yuxinwang9712@gmail.com>
2021-03-15 07:05:04 +0800

    interactive add calculate_sde and test

--- src/model_code/interactive_fixed_effect.py
+++ src/model_code/interactive_fixed_effect.py
@@ -91,3 +91,75 @@ class InteractiveFixedEffect:
             )
         beta_hat = B.dot(np.linalg.inv(A))
         return beta_hat
+
+    def calculate_sde(self, beta_hat, f_hat, lambda_hat):
+        """
+        Calculate Standard Error of beta_hat estimated from fit
+        """
+        a = self._calculate_a(lambda_hat)
+        M = self._calculate_M(f_hat)
+        Z = self._calculate_Z(M, a)
+        D0, D1 = self._calculate_D0_D1(beta_hat, f_hat, lambda_hat, Z)
+        sde = np.linalg.inv(D0).dot(D1).dot(np.linalg.inv(D0.T))
+        return sde
+
+    def _calculate_a(self, lambda_hat):
+        """
+        Calculate array a. Shape is (N,N)
+        """
+        A = np.linalg.inv(lambda_hat.T.dot(lambda_hat) / self._N)
+        a = np.empty(shape=(self._N, self._N))
+        for i in range(self._N):
+            for j in range(self._N):
+                a[i, j] = lambda_hat[i, :].T.dot(A).dot(lambda_hat[j, :])
+        return a
+
+    def _calculate_M(self, f_hat):
+        """
+        Calculate array M. Shape is (T,T)
+        """
+        return np.identity(self._T) - f_hat.dot(f_hat.T) / self._T
+
+    def _calculate_Z(self, M, a):
+        """
+        Calculate array Z. Shape is (p, T, N)
+        """
+        Z = np.empty((self._p, self._T, self._N))
+        for i in range(self._N):
+            Z[:, :, i] = self._exog[:, :, i].dot(M.T) - 1 / self._N * sum(
+                a[i, :]
+            ) * self._exog[:, :, i].dot(M.T)
+        return Z
+
+    def _calculate_D0_D1(self, beta_hat, f_hat, lambda_hat, Z):
+        """
+        Calculate array D0 and D1. Both shapes are (p, p)
+        """
+        sita_square = np.zeros(self._N)
+        for i in range(self._N):
+            for t in range(self._T):
+                sita_square[i] = (
+                    sita_square[i]
+                    + 1
+                    / self._T
+                    * (
+                        self._dependent[t, i]
+                        - beta_hat.dot(self._exog[:, t, i])
+                        - lambda_hat[i, :].T.dot(f_hat[t, :])
+                    )
+                    ** 2
+                )
+
+        D0 = np.zeros((self._p, self._p))
+        D1 = np.zeros((self._p, self._p))
+        for i in range(self._N):
+            for t in range(self._T):
+                A = (
+                    1
+                    / self._N
+                    / self._T
+                    * np.dot(np.transpose([Z[:, t, i]]), [Z[:, t, i]])
+                )
+                D0 = D0 + A
+                D1 = D1 + sita_square[i] * A
+        return D0, D1
